ef1e55c3a2b43bba1830d231fad50513c24257fa
batofgotham11 <virun006@gmail.com>
2022-04-21 11:40:45 +0530

    Initial commit.

---
+++ app/src/main/java/com/batofgotham/easyotp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.batofgotham.easyotp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_EasyOTP)
+
+        setContentView(R.layout.activity_main)
+    }
+}
